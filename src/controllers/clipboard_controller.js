0852394fd59225621fac76782b8d8aaba73e394f
Sherilyn Moline <smoline@gmail.com>
2022-12-10 12:54:02 -0500

    designing for resilience

--- src/controllers/clipboard_controller.js
+++ src/controllers/clipboard_controller.js
@@ -2,6 +2,13 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = [ "source" ]
+  static classes = [ "supported" ]
+
+  connect() {
+    if ("clipboard" in navigator) {
+      this.element.classList.add(this.supportedClass);
+    }
+  }
 
   copy(event) {
     event.preventDefault()
